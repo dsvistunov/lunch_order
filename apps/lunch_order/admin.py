44085918f059901ebb0730621d589b721143c97f
dsvistunov <d.svistuno0v1991@gmail.com>
2018-04-13 15:09:09 +0300

    registered user and product, used modeladmin

--- apps/lunch_order/admin.py
+++ apps/lunch_order/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
+from .models import Product
+from .forms import CsvImportForm
 
-# Register your models here.
+
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email', 'username', 'is_staff']
+    class Meta:
+        model = User
+
+
+class ProductModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "manufacturer", "price"]
+    class Meta:
+        model = Product
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.register(User, UserModelAdmin)
+admin.site.register(Product, ProductModelAdmin)
