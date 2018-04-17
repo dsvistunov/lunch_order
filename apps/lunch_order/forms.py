85ca51935144c78d56cb456aaad14cb2680a20a7
dsvistunov <d.svistuno0v1991@gmail.com>
2018-04-17 16:32:17 +0300

    form for login

--- apps/lunch_order/forms.py
+++ apps/lunch_order/forms.py
@@ -1,4 +1,12 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django import forms
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label="Login", max_length=30,
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
+    password = forms.CharField(label="Password", max_length=30,
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'password', 'type': 'password'}))
 
 
 class CsvImportForm(forms.Form):
