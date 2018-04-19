614ab15729b8745d38ffc78e6a5799c50e5b68e1
dsvistunov <d.svistuno0v1991@gmail.com>
2018-04-19 17:54:10 +0300

    added custome pagination class

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.generics import ListAPIView
 from apps.lunch_order.models import Product
 from .serializers import ProductSerializer
+from .pagination import ProductPagination
 
 
 class ProductListAPIView(ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    pagination_class = ProductPagination
