1ee76fd2c7bc71aa0077f6ec07a826a916e6a3e6
dsvistunov <d.svistuno0v1991@gmail.com>
2018-04-26 16:52:43 +0300

    product and order create serializers

--- apps/api/serializers.py
+++ apps/api/serializers.py
@@ -1,12 +1,45 @@
-from rest_framework.serializers import ModelSerializer
-from apps.lunch_order.models import Product
+import datetime
+from django.utils import timezone
+from rest_framework.serializers import ModelSerializer, PrimaryKeyRelatedField
+from apps.lunch_order.models import Product, Order
+
+
+class ProductListSerializer(ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            'id',
+            'title',
+            'manufacturer',
+            'price',
+        ]
 
 
 class ProductSerializer(ModelSerializer):
     class Meta:
         model = Product
         fields = [
-            'title',
-            'manufacturer',
-            'price',
+            'id'
         ]
+
+
+class OrderCreateSerializer(ModelSerializer):
+    product = PrimaryKeyRelatedField(source='product_set', many=True, queryset=Product.objects.all())
+
+    class Meta:
+        model = Order
+        fields = [
+            'owner',
+            'product'
+        ]
+
+    def create(self, validated_data):
+        product = validated_data.pop('product_set')
+        owner = validated_data.pop('owner')
+        order, created = Order.objects.get_or_create(
+            owner=owner,
+            date=datetime.date.today(),
+            defaults={'owner': owner}
+        )
+        order.product_set.add(product[0])
+        return order
