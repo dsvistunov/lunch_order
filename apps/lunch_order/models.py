f23932cc89b9cd6e844f26bceaf9a11f81dea2a2
dsvistunov <d.svistuno0v1991@gmail.com>
2018-04-26 17:04:27 +0300

    order model, order field to product

--- apps/lunch_order/models.py
+++ apps/lunch_order/models.py
@@ -1,7 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Order(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
 
 
 class Product(models.Model):
+    order = models.ManyToManyField(Order, blank=True)
     title = models.CharField(max_length=120)
     manufacturer = models.CharField(max_length=120)
     price = models.IntegerField()
+
+    def __str__(self):
+        return self.title
