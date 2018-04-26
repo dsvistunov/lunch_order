9ea0f8ae4b13edb0b8be4dbdae85bbc527f5dca2
dsvistunov <d.svistuno0v1991@gmail.com>
2018-04-26 16:53:35 +0300

    order create view

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,10 +1,25 @@
-from rest_framework.generics import ListAPIView
-from apps.lunch_order.models import Product
-from .serializers import ProductSerializer
+import datetime
+from rest_framework import status
+from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.response import Response
+from apps.lunch_order.models import Product, Order
+from .serializers import ProductListSerializer, OrderCreateSerializer
 from .pagination import ProductPagination
 
 
 class ProductListAPIView(ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+    queryset = Product.objects.order_by('title')
+    serializer_class = ProductListSerializer
     pagination_class = ProductPagination
+
+
+class OrderCreateAPIView(CreateAPIView):
+    model = Order
+    serializer_class = OrderCreateSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
