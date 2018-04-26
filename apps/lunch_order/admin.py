c149a9d531458d62fdfa513dfb09192099b7525b
dsvistunov <d.svistuno0v1991@gmail.com>
2018-04-26 17:05:02 +0300

    order model admin

--- apps/lunch_order/admin.py
+++ apps/lunch_order/admin.py
@@ -1,23 +1,39 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
-from .models import Product
+from .models import Product, Order
 
 
 class UserModelAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'email', 'username', 'is_staff']
+    list_display_links = ['username']
 
     class Meta:
         model = User
 
 
 class ProductModelAdmin(admin.ModelAdmin):
-    list_display = ["title", "manufacturer", "price"]
+    list_display = ["id", "title", "manufacturer", "price"]
 
     class Meta:
         model = Product
+
+
+class ProductInLine(admin.TabularInline):
+    model = Product.order.through
+
+
+class OrderModelAdmin(admin.ModelAdmin):
+    list_display = ["owner", "date", "id"]
+    inlines = [
+        ProductInLine,
+    ]
+
+    class Meta:
+        model = Order
 
 
 admin.site.unregister(User)
 admin.site.unregister(Group)
 admin.site.register(User, UserModelAdmin)
 admin.site.register(Product, ProductModelAdmin)
+admin.site.register(Order, OrderModelAdmin)
