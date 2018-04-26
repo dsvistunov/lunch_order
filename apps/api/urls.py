a9f9824f262b6b8d4e66eb056494b5caf24c8369
dsvistunov <d.svistuno0v1991@gmail.com>
2018-04-26 16:54:25 +0300

    url for create view

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import ProductListAPIView
+from .views import ProductListAPIView, OrderCreateAPIView
 
 urlpatterns = [
+    url(r'^create/$', OrderCreateAPIView.as_view(), name='create'),
     url(r'^products/', ProductListAPIView.as_view(), name='list'),
 ]
