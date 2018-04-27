23f95471a7e72d359924d10eeacddd51f19e1811
dsvistunov <d.svistuno0v1991@gmail.com>
2018-04-27 14:55:49 +0300

    removed create method and unused imports

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,6 +1,4 @@
-from rest_framework import status
 from rest_framework.generics import ListAPIView, CreateAPIView
-from rest_framework.response import Response
 from apps.lunch_order.models import Product, Order
 from .serializers import ProductListSerializer, OrderCreateSerializer
 from .pagination import ProductPagination
@@ -15,10 +13,3 @@ class ProductListAPIView(ListAPIView):
 class OrderCreateAPIView(CreateAPIView):
     model = Order
     serializer_class = OrderCreateSerializer
-
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
