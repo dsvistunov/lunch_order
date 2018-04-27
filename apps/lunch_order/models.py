4bb29a41744a5b56180a8020d7690338a7e3451c
dsvistunov <d.svistuno0v1991@gmail.com>
2018-04-27 14:06:54 +0300

    maked extra field products

--- apps/lunch_order/models.py
+++ apps/lunch_order/models.py
@@ -2,19 +2,24 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Order(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField(auto_now_add=True)
-
-    def __str__(self):
-        return str(self.id)
-
-
 class Product(models.Model):
-    order = models.ManyToManyField(Order, blank=True)
     title = models.CharField(max_length=120)
     manufacturer = models.CharField(max_length=120)
     price = models.IntegerField()
 
     def __str__(self):
         return self.title
+
+
+class Order(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, blank=True, through='OrderItem')
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
